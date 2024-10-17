91ad1710e30d46015a2b39b980b126f1df19afd5
Iris <irisgamedev.contact@gmail.com>
2024-10-17 18:34:57 -0300

    Set up of BG tilemap and bound collisions

---
+++ OrcForest/Assets/Scripts/Enemies/EnemyFacade.cs
@@ -0,0 +1,17 @@
+/*
+* Author: irisGameDev
+* GitHub: https://github.com/irisGameDev
+* Date: DD/MM/YYYY (DD/MM/YYYY)
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFacade : MonoBehaviour {
+	#region Variables
+	#endregion
+
+	#region Public methods
+	#endregion    
+}
